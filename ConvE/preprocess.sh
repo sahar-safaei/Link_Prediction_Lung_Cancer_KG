c89cc6d03af6ee8760aec55dc1e2c6db3510d983
sahar-safaei <sahar_sf93@yahoo.com>
2023-09-12 19:22:59 +0200

    Add Biomarker and Relapse KG to preprocess

--- ConvE/preprocess.sh
+++ ConvE/preprocess.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir data
+mkdir data/Biomarker
+mkdir data/Relapse
 mkdir data/WN18RR
 mkdir data/YAGO3-10
 mkdir data/FB15k-237
@@ -7,12 +9,16 @@ mkdir data/nations
 mkdir data/kinship
 mkdir data/umls
 mkdir saved_models
+tar -xvf Biomarker.tar.gz -C data/Biomarker
+tar -xvf Relapse.tar.gz -C data/Relapse
 tar -xvf WN18RR.tar.gz -C data/WN18RR
 tar -xvf YAGO3-10.tar.gz -C data/YAGO3-10
 tar -xvf FB15k-237.tar.gz -C data/FB15k-237
 tar -xvf umls.tar.gz -C data/umls
 tar -xvf kinship.tar.gz -C data/kinship
 tar -xvf nations.tar.gz -C data/nations
+python wrangle_KG.py Biomarker
+python wrangle_KG.py Relapse
 python wrangle_KG.py WN18RR
 python wrangle_KG.py YAGO3-10
 python wrangle_KG.py FB15k-237
